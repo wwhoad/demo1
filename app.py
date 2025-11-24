90e092423622a70e1a95a5f6a51a8a86c5b41ede
wwhoad <wwhoad@connect.ust.hk>
2025-11-24 14:00:45 +0800

    Change sidebar menu options in app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ st.write('This is your Python Programming course.')
 with st.sidebar:
     selected=option_menu(
         menu_title = "Menu",
-        options = ["Home", "About", "Contact"],
+        options = ["ISOM3400", "About", "Contact"],
         icons = ["house","cloud-upload","list-task"],
-        menu_icon= "emoji-smile-fill",
+        menu_icon= "cast",
         default_index=0,
     )
 
-if selected == "Home":
+if selected == "ISOM3400":
     st.title(f"Welcome to the {selected} page.")
 
 if selected == "About":
