60ea00e42dffdcf2ec6672f56c2d20b3d043a887
wwhoad <wwhoad@connect.ust.hk>
2025-11-24 13:55:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ with st.sidebar:
     selected=option_menu(
         menu_title = "Menu",
         options = ["Home", "About", "Contact"],
-        icons = ["1-circle-fill",
-                 "2-circle-fill",
-                 "3-circle-fill"],
+        icons = ["house","cloud-upload","list-task"],
         menu_icon= "emoji-smile-fill",
         default_index=0,
     )
