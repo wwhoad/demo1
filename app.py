82a05ffe5f5978006409a688795a78747d4bb79c
wwhoad <wwhoad@connect.ust.hk>
2025-11-24 14:18:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,10 +24,14 @@ if selected == "About":
 if selected == "Contact":
     st.title(f"Welcome to the {selected} page.")
 
+# Get the current working directory
 current_directory = os.getcwd()
-file_path = os.path.join(current_directory,'winequality-red.csv)
+# Define the file path
+file_path = os.path.join(current_directory, 'winequality-red.csv')
 
-df = pd.read_csv(file_path,delimiter=';')
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path, delimiter=';')
 
-st.write('Wine Quality Data')
+# Display the DataFrame in an interactive table
+st.write("Wine Quality Data")
 st.dataframe(df)
